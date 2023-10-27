e21ec85448e2bc9810d02edd2c29f78b7b972660
EstoesMoises <contactomoises1@gmail.com>
2023-10-27 12:35:48 +0100

    logic for adding tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import TaskList from "./TaskList";
+import TaskList from "./components/TaskList";
+import TaskForm from "./components/TaskForm";
+import { tasks as data } from "./data/tasks";
+import { useState, useEffect } from "react";
 
-function HolaMundo() {
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  function createTask(task) {
+    setTasks([...tasks, {
+      title: task.title,
+      id: tasks.length,
+      description: task.description
+    }]);
+  }
+
   return (
-    <div>
-      <TaskList />
-    </div>
+    <>
+      <TaskForm createTask={createTask}/>
+      <TaskList tasks={tasks} />
+    </>
   );
 }
 
-export default HolaMundo;
+export default App;
---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+function TaskForm({ createTask }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createTask({
+      title,
+      description,
+    });
+    setTitle("");
+    setDescription("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        autoFocus
+        placeholder="Write a task"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+      />
+      <textarea
+        placeholder="write description"
+        onChange={(e) => setDescription(e.target.value)}
+        value={description}
+      ></textarea>
+      <button>Save</button>
+    </form>
+  );
+}
+
+export default TaskForm;
