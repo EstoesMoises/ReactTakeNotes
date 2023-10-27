783e6e386afe28fa5e06414e94e6552aab43c78c
EstoesMoises <contactomoises1@gmail.com>
2023-10-27 21:25:46 +0100

    App functions now live in TaskContext

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,4 +1,9 @@
+import { useContext } from "react";
+import { TaskContext } from "../context/TaskContext";
+
 export default function TaskCard({ task, deleteTask }) {
+  const { deleteTask } = useContext(TaskContext);
+
   return (
     <div>
       <h1>{task.title}</h1>
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,13 +1,17 @@
 import TaskCard from "./TaskCard";
+import { useContext } from "react";
+import { TaskContext } from "../context/TaskContext";
 
 function TaskList({ tasks, deleteTask }) {
+  const { tasks } = useContext(TaskContext);
+
   if (tasks.length === 0) {
     return <h1>No hay tareas</h1>;
   }
   return (
     <div>
       {tasks.map((task) => (
-        <TaskCard key={task.id} task={task} deleteTask={deleteTask}/>
+        <TaskCard key={task.id} task={task} />
       ))}
     </div>
   );
